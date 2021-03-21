6a6e53bbd87d9e41fc3192fcd8afb3c10b24b826
K4liber <janbielecki94@gmail.com>
2021-03-21 19:59:51 +0100

    fix tests

--- project/tests/models/test_df.py
+++ project/tests/models/test_df.py
@@ -7,13 +7,13 @@ from project.models.data import get_data_frame, DataFrameColumns
 
 class TestDataFrame(TestCase):
     def test_get_data_frame(self):
-        results_filepath = join(ROOT_DIR, '..', 'execution_results/results.csv')
+        results_filepath = join(ROOT_DIR, '..', 'execution_results/test.csv')
         df, df_err = get_data_frame(results_filepath)
         self.assertEqual(df_err, None, f'get_data_frame err: {str(df_err)}')
 
     def test_get_data_frame_for_specific_app(self):
         app_id = 1
-        results_filepath = join(ROOT_DIR, '..', 'execution_results/results.csv')
+        results_filepath = join(ROOT_DIR, '..', 'execution_results/test.csv')
         df, df_err = get_data_frame(results_filepath, app_id)
         self.assertEqual(df_err, None, f'get_data_frame err: {str(df_err)}')
         self.assertEqual(len(df), 21)
